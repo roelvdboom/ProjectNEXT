d3aca7a8e0683f26fc4bfc46e79f32721ecb3db5
Ricardo de Beijer <ricardodebeijer@hotmail.com>
2017-02-08 14:50:22 +0100

    Added Classes for data binding Removed DataEnum

---
+++ PerzonalizedDictionary/PerzonalizedDictionary/Data/DataClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerzonalizedDictionary.Data
+{
+    abstract class DataClass : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+            storage = value;
+            this.OnPropertyChaned(propertyName);
+            return true;
+        }
+
+        private void OnPropertyChaned(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PerzonalizedDictionary/PerzonalizedDictionary/Data/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerzonalizedDictionary.Data
+{
+    class Person : DataClass
+    {
+        public Person(string firstname, string surname, string preposistion = null, Address address = null)
+        {
+            FirstName = firstname;
+            SurName = surname;
+            Preposition = preposistion;
+            Address = address;
+        }
+
+        private string _firstname;
+        private string _surname;
+        private string _preposition;
+        private Address _address;
+
+        public string FirstName
+        {
+            get { return _firstname; }
+
+            set { this.SetProperty(ref this._firstname, value); }
+        }
+
+        public string SurName
+        {
+            get { return _surname; }
+
+            set { this.SetProperty(ref this._surname, value); }
+        }
+
+        public string Preposition
+        {
+            get { return _preposition; }
+            set { this.SetProperty(ref this._preposition, value); }
+        }
+
+        public Address Address
+        {
+            get { return _address; }
+            set { this.SetProperty(ref this._address, value); }
+        }
+    }
+}
